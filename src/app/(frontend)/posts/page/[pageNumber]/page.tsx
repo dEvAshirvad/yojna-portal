290cb0a16eb8d7c09b36b07d6455ab1ced443000
Ashirvad Satapathy <74089931+dEvAshirvad@users.noreply.github.com>
2026-06-03 17:32:15 +0530

    feat: seo

--- src/app/(frontend)/posts/page/[pageNumber]/page.tsx
+++ src/app/(frontend)/posts/page/[pageNumber]/page.tsx
@@ -77,7 +77,7 @@ export default async function Page({ params: paramsPromise }: Args) {
 export async function generateMetadata({ params: paramsPromise }: Args): Promise<Metadata> {
   const { pageNumber } = await paramsPromise
   return {
-    title: `Payload Website Template Posts Page ${pageNumber || ''}`,
+    title: `Yojna Portal | Raipur District Government Posts Page ${pageNumber || ''}`,
   }
 }
 
